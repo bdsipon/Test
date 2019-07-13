2adf8142bf28c70ea075daca40454d144f61e99d
bdsipon <sipon.here@gmail.com>
2019-07-13 21:46:47 +1000

    updated data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,18 +1,28 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DataService {
-  private url='https://my-json-server.typicode.com/apollo-motorhomes/youtube-test/videos';
+  private url =  "https://my-json-server.typicode.com/apollo-motorhomes/youtube-test/videos";
+  
+  private user_url = "http://jsonplaceholder.typicode.com/users";
 
-  constructor() { }
+  constructor() {}
 
-
-
-  getAllVideos(){
+  getAllVideos() {
     return fetch(this.url);
   }
-}
 
+  getVideoById(id: number) {
+    return fetch(this.url + "/" + id);
+  }
+
+  getComments(id: number) {
+    return fetch(this.url + "/" + id + "/comments");
+  }
+
+  getUser(id) {
+    return fetch(this.user_url + "/" + id);
+  }
+}
